c25ff72504e4812e76c10fb81bd0dff4170c0441
Rahul7060 <rs0275645@gmail.com>
2024-03-21 16:03:03 +0530

    TMP
    Signed-off-by: Rahul7060 <rs0275645@gmail.com>

--- server/app.js
+++ server/app.js
@@ -11,7 +11,9 @@ const app = express();
 
 // Allow requests from http://localhost:3000
 app.use(cors({
-   origin: 'http://localhost:3000'
+   origin: 'https://payment-server-one.vercel.app/',
+   methods: ["POST", "GET"],
+   credentials: true
 }));
 
 app.use(express.json());
@@ -20,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", paymentRoute);
 
 app.get("/api/getkey", (req,res) =>{
-   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+   res.setHeader('Access-Control-Allow-Origin', 'https://payment-server-one.vercel.app/');
    res.status(200).json({key:process.env.RAZORPAY_API_KEY})
 });
 
